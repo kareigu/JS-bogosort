e8ef2c1e1c9c91dfd49e28ac5f4271e0b481814b
Eetu Kivelä <eetu.kivelae@gmail.com>
2020-05-27 16:10:54 +0300

    Fixed error in math

--- src/fillArray.js
+++ src/fillArray.js
@@ -1,6 +1,6 @@
 export let fillArray = (length) => {
   let output = [];
-  for (let i = 0; i < length - 1; i++) {
+  for (let i = 0; i < length; i++) {
     output[i] = i + 1;
   }
   return output;
