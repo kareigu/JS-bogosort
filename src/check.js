bfa1c2e8214d413d4951d73a12f73af364f486d9
Eetu Kivelä <eetu.kivelae@gmail.com>
2020-05-27 16:10:32 +0300

    Removed debug code

--- src/check.js
+++ src/check.js
@@ -1,7 +1,6 @@
 export let check = (sArray, tArray) => {
   for(let i = 0; i < sArray.length; i++) {
     if (sArray[i] != tArray[i]){
-      console.log(sArray[i] + ", " + tArray[i]);
       return false;
     }
   }
