7623a6f6474526f9a0a8e1a463935aa41c517a67
Eetu Kivelä <eetu.kivelae@gmail.com>
2020-05-28 13:40:16 +0300

    Added colours to log messages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { check } from "./check.js"
 import { readFlags } from "./flags.js"
 import { fillArray } from "./fillArray.js"
 import { startTimer, stopTimer } from "./timer.js"
+import clc from 'https://deno.land/x/color/index.ts'
 
 
 
 //Read startup flags and store in a value
 const arrLength = readFlags();
 console.log("Running bogosort for an array of: " + arrLength);
-console.log("----");
+console.log(clc.yellow.text("----"));
 
 //Fill reference array based on the startup flag
 const sortedArray = fillArray(arrLength);
@@ -24,9 +25,9 @@ while (!isSorted) {
   if (check(sortedArray, arrayToSort)) {
     //End the loop and log the result for reference
     isSorted = true;
-    console.log("Iterations: " + iterations);
-    console.log("Time taken: " + stopTimer());
-    console.log("Result: " + arrayToSort);
+    console.log(clc.magenta.text("Iterations: " + iterations));
+    console.log(clc.cyan.text("Time taken: " + stopTimer()));
+    console.log(clc.green.text("Result: " + arrayToSort));
   }
   iterations++;
   arrayToSort = shuffle(arrayToSort);
